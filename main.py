1a202e0b8e62756e5eaf209e2baf43d748feeaa4
kevingoh <sengtak@hotmail.com>
2020-10-13 11:37:21 +0200

    modified to make it work

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+import sys
+print(sys.version)
+
 from goosepaper import (
     Goosepaper,
     RSSFeedStoryProvider,
     WikipediaCurrentEventsStoryProvider,
     MultiTwitterStoryProvider,
     WeatherStoryProvider,
+    TwitterStoryProvider,
 )
 
 print(
     Goosepaper(
         [
-            WikipediaCurrentEventsStoryProvider(),
-            WeatherStoryProvider(woe="2358820"),
-            RSSFeedStoryProvider("https://www.statnews.com/feed/", limit=5),
-            RSSFeedStoryProvider("https://www.npr.org/feed/", limit=5),
-            MultiTwitterStoryProvider(
-                ["reuters", "bbcWorld", "axios", "BethanyAllenEbr", "NPR"], limit_per=5
-            ),
+            #WikipediaCurrentEventsStoryProvider(),
+            #WeatherStoryProvider(woe="2358820"),
+            #RSSFeedStoryProvider("https://news.yahoo.com/rss/world", limit=10),
+            RSSFeedStoryProvider("https://rss.bloople.net/?url=https%3A%2F%2Fnews.yahoo.com%2Frss%2Fmostviewed&showtitle=false&type=js", limit=5),
+            #MultiTwitterStoryProvider(
+                #["reuters", "bbcWorld"], limit_per=5
+            #),
+            #TwitterStoryProvider("bbcWorld",limit=5),
         ]
     ).to_html()
 )
